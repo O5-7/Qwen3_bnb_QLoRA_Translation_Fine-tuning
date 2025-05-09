952936c57a0b13a46fb369494937afe87a211094
Normal1919 <517205163@qq.com>
2025-05-10 00:44:40 +0800

    Init

---
+++ stream.py
@@ -0,0 +1,56 @@
+class CustomTextStreamer(TextStreamer):
+    def __init__(self, tokenizer, skip_prompt=True, skip_special_tokens=True):
+        super().__init__(tokenizer, skip_prompt=skip_prompt, skip_special_tokens=skip_special_tokens)
+        self.generated_text = ""
+        self.stop_flag = False
+
+    def on_finalized_text(self, text: str, stream_end: bool = False):
+        self.generated_text += text
+        print(text, end="", flush=True)
+        if self.stop_flag:
+            raise StopIteration
+
+    def stop_generation(self):
+        self.stop_flag = True
+
+
+def generate_stream(model, tokenizer, messages, enable_thinking, skip_prompt, skip_special_tokens, max_new_tokens):
+    input_ids = tokenizer.apply_chat_template(
+        messages,
+        tokenize=True,
+        enable_thinking=enable_thinking,
+        add_generation_prompt=True,
+        return_tensors="pt"
+    )
+    attention_mask = torch.ones_like(input_ids, dtype=torch.long)
+    tokens = input_ids.to(model.device)
+    attention_mask = attention_mask.to(model.device)
+
+    streamer = CustomTextStreamer(tokenizer, skip_prompt=skip_prompt, skip_special_tokens=skip_special_tokens)
+
+    def signal_handler(sig, frame):
+        streamer.stop_generation()
+        print("\n[Generation stopped by user with Ctrl+C]")
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    print("Response: ", end="", flush=True)
+    try:
+        generated_ids = model.generate(
+            tokens,
+            attention_mask=attention_mask,
+            use_cache=False,
+            max_new_tokens=max_new_tokens,
+            do_sample=True,
+            pad_token_id=tokenizer.pad_token_id,
+            streamer=streamer
+        )
+        del generated_ids
+    except StopIteration:
+        print("\n[Stopped by user]")
+
+    del input_ids, attention_mask
+    torch.cuda.empty_cache()
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    return streamer.generated_text, streamer.stop_flag
